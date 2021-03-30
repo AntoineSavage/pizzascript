d2a6996775bda51205f584cebafd2b9f9ffd6324
Antoine Savage <antoine.savage@gmail.com>
2021-03-30 16:13:54 -0400

    Clean-up vscode compile errors

---
+++ src/Data/Atom.hs
@@ -0,0 +1,7 @@
+module Data.Atom (Atom) where
+
+data Atom
+    = AtomNone
+    | AtomFalse
+    | AtomTrue
+    | Atom String
---
+++ test/Data/AtomSpec.hs
@@ -0,0 +1,17 @@
+module Data.AtomSpec where
+
+import Data.Atom
+import Test.Hspec
+import Test.QuickCheck
+
+spec :: Spec
+spec = do
+  describe "Prelude.head" $ do
+    it "returns the first element of a list" $ do
+      head [23 ..] `shouldBe` (23 :: Int)
+
+    it "returns the first element of an *arbitrary* list" $
+      property $ \x xs -> head (x:xs) == (x :: Int)
+
+    it "throws an exception if used with an empty list" $ do
+      evaluate (head []) `shouldThrow` anyException
