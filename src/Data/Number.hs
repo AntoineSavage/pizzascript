08c7f05350ee5bd187413866a89fdab18d803128
Antoine Savage <antoine.savage@gmail.com>
2021-04-02 09:56:36 -0400

    Number data type

--- src/Data/Number.hs
+++ src/Data/Number.hs
@@ -1,4 +1,4 @@
-module Data.Number (Number(..), parser) where
+module Data.Number (Number(..), unparse, parser) where
 
 import Control.Monad ( liftM2 )
 import Text.Parsec
@@ -7,11 +7,23 @@ import Utils
 
 data Number
     = NumberInt Int
-    | NumberDouble Double 
+    | NumberFloat Double
     deriving (Eq, Ord, Show, Read)
 
+unparse :: Number -> String
+unparse number =
+    case number of
+        NumberInt n -> show n
+        NumberFloat d -> show d
+
 parser :: Parser Number
-parser =
+parser = do
     let uint = many1 $ oneOf digits
         sint = liftM2 (++) (option "" $ string "-") uint
-    in NumberInt . read <$> sint
+        option_or_cons cp sp = option "" $ liftM2 (:) cp sp
+    intPart <- sint
+    decPart <- option_or_cons (char '.') uint
+    expPart <- option_or_cons (oneOf "eE") sint
+    return $ case (decPart, expPart) of
+        ("", "") -> NumberInt $ read intPart
+        (d, e) -> NumberFloat $ read $ intPart ++ decPart ++ expPart
