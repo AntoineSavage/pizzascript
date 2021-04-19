3c86b056030714939fafafa733b446b657234ac2
Antoine Savage <antoine.savage@gmail.com>
2021-04-19 17:59:57 -0400

    Parse symbol

---
+++ src/Data/PzSymb.hs
@@ -0,0 +1,16 @@
+module Data.PzSymb (PzSymb(..), parser, unparse) where
+
+import Control.Monad ( liftM2 )
+import qualified Data.PzIdent as I
+import Text.Parsec ( char, many1 )
+import Text.Parsec.String (Parser)
+
+data PzSymb
+    = PzSymb Int I.PzIdent
+    deriving (Show, Eq)
+
+parser :: Parser PzSymb
+parser = liftM2 PzSymb (length <$> many1 (char '\'')) I.parser
+
+unparse :: PzSymb -> String
+unparse (PzSymb n ident) = replicate (max 1 n) '\'' ++ I.unparse ident
---
+++ test/Data/PzSymbSpec.hs
@@ -0,0 +1,51 @@
+module Data.PzSymbSpec where
+
+import Test.Hspec
+import Test.QuickCheck
+
+import Control.Monad
+import Data.Either
+import qualified Data.PzIdent as I
+import Data.PzIdentSpec ()
+import Data.PzSymb
+import Text.Parsec
+
+spec :: Spec
+spec = do
+    parseVsUnparseSpec
+    parseSpec
+    unparseSpec
+
+parseVsUnparseSpec :: Spec
+parseVsUnparseSpec = describe "parse vs unparse" $ do
+    it "composes parse and unparse" $ do
+        property $ \(Positive n) ident -> do
+            let s = unparse (PzSymb n ident)
+            parse parser "tests" s `shouldBe` Right (PzSymb n ident)
+            unparse <$> parse parser "tests" s `shouldBe` Right (unparse $ PzSymb n ident)
+
+parseSpec :: Spec
+parseSpec = describe "parse" $ do
+    it "rejects empty string" $ do
+        isLeft (parse parser "tests" "") `shouldBe` True
+
+    it "parses single quote followed by ident" $ do
+        property $ \ident -> do
+            let s = '\'' : I.unparse ident
+            parse parser "tests" s `shouldBe` Right (PzSymb 1 ident)
+
+    it "parses n quotes followed by ident" $ do
+        property $ \(Positive n) ident -> do
+            let s = replicate n '\'' ++ I.unparse ident
+            parse parser "tests" s `shouldBe` Right (PzSymb n ident)
+
+unparseSpec :: Spec
+unparseSpec = describe "unparse" $ do
+    it "unparses single quote followed by ident" $ do
+        property $ \(Negative n) ident -> do
+            unparse (PzSymb n ident) `shouldBe` '\'' : I.unparse ident
+            unparse (PzSymb 0 ident) `shouldBe` '\'' : I.unparse ident
+    
+    it "unparses n quotes followed by ident" $ do
+        property $ \(Positive n) ident -> do
+            unparse (PzSymb n ident) `shouldBe` replicate n '\'' ++ I.unparse ident
