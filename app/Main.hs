a6708efbbdbd9968fbcbc73faf5462fa745b7c60
Antoine Savage <antoine.savage@gmail.com>
2021-04-20 18:57:52 -0400

    Main!

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,11 @@
 module Main where
 
+import Ast
+import Text.Parsec.String
+
 main :: IO ()
-main = putStrLn "Hello World!"
+main = do
+    mresult <- parseFromFile (parser Ast.ignore) "example/lex.pz"
+    case mresult of
+        Left err -> print err
+        Right result -> print result
