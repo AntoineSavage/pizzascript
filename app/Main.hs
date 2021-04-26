ff8b3f57cc667c824281778418d9904b9d22d3df
Antoine Savage <antoine.savage@gmail.com>
2021-04-26 17:58:02 -0400

    Eval main.pz

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ import Text.Parsec.String ( parseFromFile )
 
 main :: IO ()
 main = do
-    mast <- parseFromFile A.parseAst "example/ast.pz"
+    mast <- parseFromFile A.parseAst "example/main.pz"
     case mast of
         Left err -> print err
         Right ast -> evalAst ast
