2a5a302bd5164be1ba13368820ff23853d6187b8
Antoine Savage <antoine.savage@gmail.com>
2021-05-24 18:14:48 -0400

    Stack frame

---
+++ src/Data/StackFrame.hs
@@ -0,0 +1,29 @@
+module Data.StackFrame ( StackFrame(..), StackFrameSpec(..), setCtx ) where
+
+import Data.Func ( Func )
+import Data.PzVal ( Dict, PzVal )
+import Data.Symb ( Symb )
+
+data StackFrame
+    = StackFrame Dict StackFrameSpec
+    deriving (Show, Eq)
+
+type Stmts = [PzVal]
+
+type FuncSymb = Maybe Symb
+type RemArgs = [PzVal]
+
+type ImplCtx = Dict
+type InvocFunc = Func PzVal
+type RdyArgs = [PzVal]
+
+data StackFrameSpec
+    = Block Stmts
+    | Form FuncSymb RemArgs
+    | Invoc FuncSymb ImplCtx InvocFunc RdyArgs (Maybe RemArgs)
+    deriving (Show, Eq)
+
+setCtx :: Dict -> [StackFrame] -> [StackFrame]
+setCtx ctx frames = case frames of
+    [] -> []
+    StackFrame _ spec : fs -> StackFrame ctx spec :fs
