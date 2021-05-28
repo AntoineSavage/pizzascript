ec00d5f6beb591aceadc9bf53210ddee96d4a941
Antoine Savage <antoine.savage@gmail.com>
2021-05-28 14:17:48 -0400

    StackFrame coverage

--- test/Ops/StackFrameSpec.hs
+++ test/Ops/StackFrameSpec.hs
@@ -9,6 +9,7 @@ import Ops.StackFrame
 import TestUtils
 import Types.PzValSpec
 import Types.StackFrame
+import Types.StackFrameSpec
 
 spec :: Spec
 spec = do
@@ -31,21 +32,3 @@ setCtxSpec = describe "setCtx" $ do
         property $ \(ArbDict ctx) mfs f (Few as) (Few es) (Few fs) -> do
             setCtx ctx (StackFrame undefined (Invoc mfs ctx f as $ Just es) :fs) `shouldBe` StackFrame ctx (Invoc mfs ctx f as $ Just es) :fs
             setCtx ctx (StackFrame undefined (Invoc mfs ctx f as Nothing) :fs) `shouldBe` StackFrame ctx (Invoc mfs ctx f as Nothing) :fs
-
--- Utils
-instance Arbitrary StackFrame where arbitrary = arbDepth
-instance ArbWithDepth StackFrame where arbWithDepth depth = liftM2 StackFrame arbDepth arbDepth
-
-instance Arbitrary StackFrameSpec where arbitrary = arbDepth
-instance ArbWithDepth StackFrameSpec where
-    arbWithDepth depth = oneof
-        [ fmap Block arbDepth
-        , liftM3 Form arbitrary arbDepth arbDepth
-        , do
-            a <- arbitrary
-            b <- arbDepth
-            c <- arbDepth
-            d <- arbDepth
-            e <- arbDepth
-            return $ Invoc a b c d e
-        ]
---
+++ test/Types/StackFrameSpec.hs
@@ -0,0 +1,84 @@
+module Types.StackFrameSpec where
+
+import Test.Hspec
+import Test.QuickCheck
+
+import Control.Monad
+import TestUtils
+import Types.FuncSpec
+import Types.PzVal
+import Types.PzValSpec
+import Types.StackFrame
+
+spec :: Spec
+spec = do
+    describe "StackFrame" $ do
+        it "implements Show" $ do
+            property $ \(ArbDict d) s -> do
+                show (StackFrame d s) `shouldBe` "StackFrame (" ++ show d ++ ") (" ++ show s ++ ")"
+
+        it "implements Eq" $ do
+            property $ \(ArbDict dx) (ArbDict dy) sx sy -> do
+                StackFrame dx sx == StackFrame dx sx `shouldBe` True
+                StackFrame dx sx == StackFrame dy sx `shouldBe` dx == dy
+                StackFrame dx sx == StackFrame dx sy `shouldBe` sx == sy
+
+    describe "StackFrameSpec" $ do
+        it "implements Show" $ do
+            property $ \s x (Few xs) (ArbDict d) f -> x /= PzUnit ==> do
+                show (Block xs) `shouldBe` "Block " ++ show xs
+                
+                show (Form Nothing PzUnit xs) `shouldBe` "Form Nothing PzUnit " ++ show xs
+                show (Form (Just s) x xs) `shouldBe` "Form (" ++ show (Just s) ++ ") (" ++ show x ++ ") " ++ show xs
+
+                show (Invoc Nothing d f xs Nothing) `shouldBe` "Invoc Nothing (" ++ show d ++ ") (" ++ show f ++ ") " ++ show xs ++ " Nothing"
+                show (Invoc (Just s) d f xs (Just xs)) `shouldBe` "Invoc (" ++ show (Just s) ++ ") (" ++ show d ++ ") (" ++ show f ++ ") " ++ show xs ++ " (" ++ show (Just xs) ++ ")"
+
+        it "implements Eq" $ do
+            property $ \msx msy x y (Few xs) (Few mxs') (Few ys) (Few mys') (ArbDict dx) (ArbDict dy) fx fy -> do
+                Block xs == Block xs `shouldBe` True
+                Block xs == Block ys `shouldBe` xs == ys
+                Block u == Form u u u `shouldBe` False
+                Block u == Invoc u u u u u `shouldBe` False
+
+                Form u u u == Block u `shouldBe` False
+                Form msx x xs == Form msx x xs `shouldBe` True
+                Form msx x xs == Form msy x xs `shouldBe` msx == msy
+                Form msx x xs == Form msx y xs `shouldBe` x == y
+                Form msx x xs == Form msx x ys `shouldBe` xs == ys
+
+                let mxs = joinListMaybe mxs'
+                    mys = joinListMaybe mys'
+                Invoc u u u u u == Block u `shouldBe` False
+                Invoc u u u u u == Form u u u `shouldBe` False
+                Invoc msx dx fx xs mxs == Invoc msx dx fx xs mxs `shouldBe` True
+                Invoc msx dx fx xs mxs == Invoc msy dx fx xs mxs `shouldBe` msx == msy
+                Invoc msx dx fx xs mxs == Invoc msx dy fx xs mxs `shouldBe` dx == dy
+                Invoc msx dx fx xs mxs == Invoc msx dx fy xs mxs `shouldBe` fx == fy
+                Invoc msx dx fx xs mxs == Invoc msx dx fx ys mxs `shouldBe` xs == ys
+                Invoc msx dx fx xs mxs == Invoc msx dx fx xs mys `shouldBe` mxs == mys
+
+-- Utils
+joinListMaybe :: [Maybe a] -> Maybe [a]
+joinListMaybe = go [] where
+    go acc []     = Just $ reverse acc
+    go acc (x:xs) = case x of
+        Nothing -> Nothing
+        Just y -> go (y:acc) xs
+
+instance Arbitrary StackFrame where arbitrary = arbDepth
+instance ArbWithDepth StackFrame where arbWithDepth depth = liftM2 StackFrame arbDepth arbDepth
+
+instance Arbitrary StackFrameSpec where arbitrary = arbDepth
+instance ArbWithDepth StackFrameSpec where
+    arbWithDepth depth = oneof
+        [ fmap Block arbDepth
+        , liftM3 Form arbitrary arbDepth arbDepth
+        , do
+            a <- arbitrary
+            b <- arbDepth
+            c <- arbDepth
+            d <- arbDepth
+            e <- arbDepth
+            return $ Invoc a b c d e
+        ]
