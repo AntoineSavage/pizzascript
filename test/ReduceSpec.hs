5cca8ecffe04877624a273cffc0b4fb35dcfa91c
Antoine Savage <antoine.savage@gmail.com>
2021-06-12 16:04:27 -0400

    Write pending reduce tests

--- test/ReduceSpec.hs
+++ test/ReduceSpec.hs
@@ -12,7 +12,106 @@ import Types.StackFrameSpec
 
 spec :: Spec
 spec = do
+    reduceSpec
+    reduceBlockSpec
+    reduceFormQuotedSpec
+    reduceFormEvaledSpec
+    reduceInvocArgsSpec
+    reduceInvocEvaledSpec
+    reduceInvocSpec
     toAccSpec
+
+reduceSpec :: Spec
+reduceSpec = describe "reduce" $ do
+    it "handles empty stack frames" $ do
+        pending
+
+    it "reduces block" $ do
+        pending
+
+    it "reduces form (quoted)" $ do
+        pending
+
+    it "reduces form (evaled)" $ do
+        pending
+
+    it "reduces invoc (quoted)" $ do
+        pending
+
+    it "reduces invoc (args)" $ do
+        pending
+
+    it "reduces invoc (evaled)" $ do
+        pending
+
+reduceBlockSpec :: Spec
+reduceBlockSpec = describe "reduceBlock" $ do
+    it "handles empty block" $ do
+        pending
+
+    it "evals next block element" $ do
+        pending
+
+reduceFormQuotedSpec :: Spec
+reduceFormQuotedSpec = describe "reduceFormQuoted" $ do
+    it "evals first form element" $ do
+        pending
+
+    it "handles evaled first form element" $ do
+        pending
+
+reduceFormEvaledSpec :: Spec
+reduceFormEvaledSpec = describe "reduceFormEvaled" $ do
+    it "rejects malformed function invocation" $ do
+        pending
+
+    it "handles eval function invocation" $ do
+        pending
+
+    it "handles quote function invocation" $ do
+        pending
+
+    it "rejects unquote function invocation" $ do
+        pending
+
+    it "rejects deep-quote function invocation" $ do
+        pending
+
+    it "rejects deep-unquote function invocation" $ do
+        pending
+
+reduceInvocArgsSpec :: Spec
+reduceInvocArgsSpec = describe "reduceInvocArgs" $ do
+    it "handles single evaluated arg" $ do
+        pending
+
+    it "handles all evaluated args" $ do
+        pending
+
+    it "evaluates single arg" $ do
+        pending
+
+reduceInvocEvaledSpec :: Spec
+reduceInvocEvaledSpec = describe "reduceInvocEvaled" $ do
+    it "invokes function" $ do
+        pending
+
+    it "handles pure function output" $ do
+        pending
+
+    it "handles impure valid function output" $ do
+        pending
+
+    it "rejects impure invalid function output" $ do
+        pending
+
+reduceInvocSpec :: Spec
+reduceInvocSpec = describe "reduceInvoc" $ do
+    it "handles built-in function invocation" $ do
+        pending
+
+    it "handles custom function invocation" $ do
+        pending
 
 toAccSpec :: Spec
 toAccSpec = describe "toAcc" $ do
